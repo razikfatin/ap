8a2558066bacd133ae848861961ac2b693de0d28
razikfatin <66723423+razikfatin@users.noreply.github.com>
2021-09-10 12:54:06 +0530

    Add files via upload

---
+++ firebaseServiceConfig.js
@@ -0,0 +1,23 @@
+//const prodConfig = {
+    // apiKey           : "YOUR_API_KEY",
+    // authDomain       : "your-app.firebaseapp.com",
+    // databaseURL      : "https://your-app.firebaseio.com",
+    // projectId        : "your-app",
+    // storageBucket    : "your-app.appspot.com",
+    // messagingSenderId: "YOUR_MESSAGING_SENDER_ID"
+//};
+
+const devConfig = {
+    apiKey: "AIzaSyBp-mi4ptUEP2-awCNnNNbiOgkBT5LE0UI",
+    authDomain: "fire-36558.firebaseapp.com",
+    databaseURL: "https://fire-36558-default-rtdb.firebaseio.com",
+    projectId: "fire-36558",
+    storageBucket: "fire-36558.appspot.com",
+    messagingSenderId: "295989997842",
+    appId: "1:295989997842:web:18a1035f508a21a19783cb",
+    measurementId: "G-DWPMMB8NEK"
+};
+
+const config = devConfig;
+
+export default config;
